be2977c54027e09e9192dba89d9062a784d6b874
Yaşar Kiremitci <yasar.krmtc@gmail.com>
2024-12-11 01:39:54 +0300

    second onboarding finish

--- app/src/main/java/com/plantapphubx/ui/paywall/PaywallFragment.kt
+++ app/src/main/java/com/plantapphubx/ui/paywall/PaywallFragment.kt
@@ -1,25 +1,18 @@
-package com.plantapphubx.ui.onboardings
+package com.plantapphubx.ui.paywall
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
-import com.plantapphubx.R
 import com.plantapphubx.base.BaseFragment
-import com.plantapphubx.databinding.FragmentSecondOnboardingBinding
+import com.plantapphubx.databinding.FragmentPaywallBinding
 import com.plantapphubx.ui.MainActivity
-import com.plantapphubx.utils.clickWithDebounce
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SecondOnboardingFragment :
-    BaseFragment<FragmentSecondOnboardingBinding>(FragmentSecondOnboardingBinding::inflate) {
+class PaywallFragment :
+    BaseFragment<FragmentPaywallBinding>(FragmentPaywallBinding::inflate) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            continueButton.clickWithDebounce {
-                findNavController().navigate(R.id.action_secondOnboardingFragment_to_paywallFragment)
-            }
-
         }
     }
     override fun onResume() {
